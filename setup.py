071c62a08b83cd816cd66ff98d19c0e90e24b924
svdguseIt <svdguseit@gmail.com>
2025-11-16 00:00:28 +0530

    Add folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Medical Chatbot Project',
+    version='0.0.0',
+    author='Vidya Gamaethige',
+    author_email='vidgamaethige@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
